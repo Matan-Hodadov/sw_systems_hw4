438601519f7b50482cfd7c0345fd2a542541dd02
Matan-Hodadov <matanhod1@gmail.com>
2021-01-16 16:22:56 +0200

    done read word function

--- tree.h
+++ tree.h
@@ -13,9 +13,10 @@ typedef struct node {
 
 node* create_node(char letter); 
 
-void add_child_to_node(node *pnode,char letter); 
+void add_child_to_node(node *pnode, char letter); 
 
-void remove_child_of_node(node *pnode,char letter);
+void remove_child_of_node(node *pnode, char letter);
 
 void dealocate_children(node *pnode); 
 
+void read_word(node *root_node, char *word);
--- treeFunction.c
+++ treeFunction.c
@@ -26,7 +26,7 @@ node* create_node(char letter)
     return pnode;
 }
 
-void add_child_to_node(node *pnode,char letter)
+void add_child_to_node(node *pnode, char letter)
 {
     int child_index = letter - 'a';
     node *pchild = *(n->children[child_index]);
@@ -36,7 +36,7 @@ void add_child_to_node(node *pnode,char letter)
     }
 }
 
-void remove_child_of_node(node *pnode,char letter)
+void remove_child_of_node(node *pnode, char letter)
 {
     int child_index = letter - 'a';
     node *pchild = *(n->children[child_index]);
@@ -58,4 +58,25 @@ void dealocate_children(node *pnode)
         }
     }
     free(pnode);
+}
+
+void read_word(node *root_node, char *word)
+{
+    node *temp = root_node; 
+    char letter;
+    node *pchild;
+    for(int i = 0; i < (int)strlen(word); i++)
+    {
+        letter = *(word + i);
+        pchild  = temp->children[i-'a'];
+        if(pchild == NULL)
+        {
+            add_child_to_node(temp, letter);
+        }
+        temp = temp->children[i-'a'];
+        if((int)strlen(word) == i)
+        {
+            temp->count = temp->count + 1;
+        }
+    }
 }
